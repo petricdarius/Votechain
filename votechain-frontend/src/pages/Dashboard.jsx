81f6bec67e583365797f983758ad8e25aff6c617
HolczliAndrei <holczli_andrei@yahoo.com>
2026-05-06 15:06:02 +0300

    afisare election cards in dashboard

--- votechain-frontend/src/pages/Dashboard.jsx
+++ votechain-frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,25 @@
+import useElections from "../hooks/useElections";
+
 function Dashboard() {
+  const { elections, isLoading } = useElections();
+  console.log(elections);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div>
-      <h2>Dashboard</h2>
+      {elections.data.docs.map((election) => (
+        <ElectionCard key={election.id} election={election} />
+      ))}
+    </div>
+  );
+}
+
+function ElectionCard({ election }) {
+  return (
+    <div className="bg-white/10 p-4 rounded-lg shadow-md">
+      <h3>{election.name}</h3>
+      <p>{election.description}</p>
     </div>
   );
 }
